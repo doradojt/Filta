364abc719dbf8770c1cb2bb6d52ab15399dc89ed
John Michals <jmichals@Dorado.Local>
2019-01-08 09:29:42 -0500

    changes

---
+++ customerData.py
@@ -0,0 +1,290 @@
+var customerData = {
+    "essex":{
+        "Professional":  {
+            "location": [40.739157, -74.327442],
+            name: "Dun & Bradstreet",
+            address: "103 John F. Kennedy Pkwy",
+            city: "Short Hills, NJ 07078"
+            },
+        {
+        location: [40.78391, -74.3143],
+        name: "FLIK Hospitality Group",
+        city: "Livingston, NJ 07039"
+        },
+        {
+        location: [40.73478, -74.1648],
+        name: "Horizon Blue Cross Blue Shield",
+        address: "3 Penn Plaza",
+        city: "Newark, NJ 07105"
+        },
+        {
+        location: [40.81149, -74.204340],
+        name: "Mountainside Hospital",
+        address: "1 Bay Avenue",
+        city: "Montclair, NJ 07028"
+        },
+        {
+        location: [40.8623, -74.1978],
+        name: "Montclair State Diner",
+        address: "1 Quad Road",
+        city: "Montclair, NJ 07043"
+        },
+        {
+        location: [40.7857, -74.1764],
+        name: "Clara Maass Medical Center",
+        address: "1 Clara Maass Drive",
+        city: "Belleville, NJ 07109"
+        },
+        {
+        location: [40.8623, -74.1978],
+        name: "Montclair State Freeman",
+        address: "1 Quad Road",
+        city: "Montclair, NJ 07043"
+        },
+        {
+        location: [40.8623, -74.1978],
+        name: "Montclair State Student Center",
+        address: "1 Quad Road",
+        city: "Montclair, NJ 07043"
+        },
+        {
+        location: [40.8623, -74.1978],
+        name: "Montclair State Chilis",
+        address: "1 Quad Road",
+        city: "Montclair, NJ 07043"
+        },
+        {
+        location: [40.8623, -74.1978],
+        name: "Montclair State Rathskeller",
+        address: "1 Quad Road",
+        city: "Montclair, NJ 07043"
+        },
+        {
+        location: [40.8623, -74.1978],
+        name: "Montclair State University",
+        address: "1 Quad Road",
+        city: "Montclair, NJ 07043"
+        },
+        {
+        location: [40.739330, -74.17030],
+        name: "Prudential Tower",
+        address: "655 Broad Street",
+        city: "Newark, NJ 07102"
+        },
+        {
+        location: [40.7692, -74.2809],
+        name: "Turtle Back Zoo",
+        address: "560 Northfield Avenue",
+        city: "West Orange, NJ 07052"
+        }];
+
+        var essexHospitals = [
+        { 
+        location: [40.7947, -74.2649],
+        name: "East Orange General Hospital",
+        address: "300 Central Avenue",
+        city: "East Orange, NJ",
+        beds: "196"
+        },
+        {
+        location: [40.7858, -74.2771],
+        name: "Kessler Institute of Rehab West Orange",
+        address: "1199 Pleasant Valley Way",
+        city: "West Orange, NJ",
+        beds: "152"
+        },
+        {
+        location: [40.70999, -74.21362],
+        name: "Newark Beth Israel Medical Center",
+        address: "201 Lyons Avenue",
+        city: "Newark, NJ",
+        beds: "480"
+        },
+        {
+        location: [40.7636, -74.3049],
+        name: "Saint Barnabas Medical Center",
+        address: "94 Old Short Hills Road",
+        city: "Livingston, NJ",
+        beds: "577"
+        },
+        {
+        location: [40.7439, -74.1746],
+        name: "Saint Michael's Medical Center",
+        address: "111 Central Avenue",
+        city: "Newark, NJ",
+        beds: "358"
+        },
+        {
+        location: [40.7281, -74.1636],
+        name: "Saint James Hospital",
+        address: "155 Jefferson Street",
+        city: "Newark, NJ",
+        beds: "189"
+        },
+        {
+        location: [40.77, -74.21],
+        name: "Brookhaven Health Center",
+        address: "120 Park End Place",
+        city: "East Orange, NJ",
+        beds: "122"
+        },
+        {
+        location: [40.7403, -74.1902],
+        name: "UMDNJ University Hospital",
+        address: "150 Bergen Street",
+        city: "Newark, NJ",
+        beds: "518"
+        },
+        {
+        location: [40.7525, -74.2361],
+        name: "VA NJ Health Care East Orange",
+        address: "385 Tremont Avenue",
+        city: "East Orange, NJ",
+        beds: "950"
+        },
+        {
+        location: [40.7659, -74.2118],
+        name: "White House Healthcare & Rehab Center",
+        address: "560 Berkeley Avenue",
+        city: "East Orange, NJ",
+        beds: "176"  
+        }
+        ];
+
+        var bergenHospitals = [
+        { 
+        location: [40.90486, -73.955410],
+        name: "Englewood Hospital",
+        address: "350 Engle Street",
+        city: "Englewood, NJ",
+        beds: "282"
+        },
+        {
+        location: [40.985210, -74.015210],
+        name: "Hackensack University M.C. Pascack Valley",
+        address: "250 Old Hook Road",
+        city: "Westwood, NJ",
+        beds: "128"
+        },
+        {
+        location: [40.876600, -74.224240],
+        name: "Hackensack University Medical Center",
+        address: "30 Prospect Avenue",
+        city: "Hackensack, NJ",
+        beds: "770"
+        },
+        {
+        location: [40.88192, -74.009086],
+        name: "Holy Name Medical Center",
+        address: "718 Teaneck Road",
+        city: "Teaneck, NJ",
+        beds: "361"
+        },
+        {
+        location: [40.895830, -74.092230],
+        name: "Kessler Institute of Rehab SaddleBrook",
+        address: "300 Market Street",
+        city: "Saddle Brook, NJ",
+        beds: "112"
+        },
+        {
+        location: [40.958920, -74.063130],
+        name: "New Bridge Medical Center",
+        address: "230 East Ridgewood Avenue",
+        city: "Paramus, NJ",
+        beds: "323"
+        },
+        {
+        location: [40.9837, -74.1008],
+        name: "Valley Hospital",
+        address: "223 N. Van Diem Avenue",
+        city: "Ridgewood, NJ",
+        beds: "451"
+        },
+        ];
+
+        var essexColleges = [
+        {
+        location: [40.741388, -74.190352],
+        name: "Rutgers University Dental School",
+        address: "110 Bergen Street",
+        students: "375",
+        city: "Newark, NJ"
+        },
+        {
+        location: [40.743447, -74.191798],
+        name: "Rutgers University Health Related Professionals",
+        address: "65 Bergen Street",
+        students: "1800",
+        city: "Newark, NJ"
+        },
+        {
+        location: [40.744599, -74.170841],
+        name: "Rutgers University School of Public Affairs and Administration",
+        address: "111 Washington Street",
+        students: "1200",
+        city: "Newark, NJ"
+        },
+        {
+        location: [40.795, -74.195],
+        name: "Bloomfield College",
+        address: "467 Franklin Street",
+        students: "1947",
+        city: "Bloomfield, NJ"
+        },
+        {
+        location: [40.834, -74.273],
+        name: "Caldwell University",
+        address: "120 Bloomfield Avenue",
+        students: "2200",
+        city: "Caldwell, NJ"
+        },
+        {
+        location: [40.7387,-74.1784],
+        name: "Essex County Community College",
+        address: "303 University Avenue",
+        students: "8175",
+        city: "Newark, NJ"
+        },
+        {
+        location: [40.738890, -74.190730],
+        name: "Rutgers University Medical School",
+        address: "185 S. Orange Avenue",
+        students: "680",
+        city: "Newark, NJ"
+        },
+        {
+        location: [40.736670, -74.166290],
+        name: "Seton Hall University School of Law",
+        address :"1109 Raymond Blvd",
+        students: "734",
+        city: "Newark, NJ"
+        },
+        ];
+
+        var airports = [
+        {
+        location: [40.6895, -74.1745],
+        name: "Newark Liberty International Airport",
+        address: "3 Brewster Road",
+        city: "Newark, NJ 07114"
+        },
+        {
+        location: [40.875278, -74.1653],
+        name: "Essex County Airport",
+        address: "Building M, 27 Wright Way",
+        city: "Fairfield, NJ 07004"
+        },
+        {
+        location: [40.9475, -74.31444],
+        name: "Lincoln Park Airport",
+        address: "425 Beaverbrook Road",
+        city: "Lincoln, NJ 07035"
+        },
+        {
+        location:[40.85, -74.060833],
+        name: "Teterboro Airport",
+        address: "111 Industrial Avenue",
+        city: "Teterboro, NJ 07608"
+        },
+        ]
